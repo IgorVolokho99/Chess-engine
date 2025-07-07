7896cd068290a743304d2caf95ede63407335f20
Igor Volokho <igorvolokho99@gmail.com>
2025-07-08 02:29:19 +0300

    #2 New class FenState.

---
+++ src/fen_state.py
@@ -0,0 +1,56 @@
+"""Модуль, который содержит реализацию и хранения параметров fen-нотации."""
+from src.coord import Coord
+from src.enums import Color
+
+from_letter_to_digit = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
+
+
+class FenState:
+    """Реализация fen-параметров."""
+
+    def __init__(self, fen: str) -> None:
+        """Инициализирует поля объектами None."""
+        self.board_part = None
+        self.active_color = None
+        self.white_long_castling = None
+        self.white_short_castling = None
+        self.black_long_castling = None
+        self.black_short_castling = None
+        self.en_passant_cell = None
+        self.moves_without_pawn = None
+        self.move_clock = None
+
+        self.parse_fen(fen)
+
+    def parse_fen(self, fen: str) -> None:
+        fen_parts = fen.split(' ')
+
+        self.board_part = fen_parts[0]
+
+        if fen_parts[1] == 'w':
+            self.active_color = Color.white
+        else:
+            self.active_color = Color.black
+
+        if 'K' in fen_parts[2]:
+            self.white_short_castling = True
+        if 'Q' in fen_parts[2]:
+            self.white_long_castling = True
+        if 'k' in fen_parts[2]:
+            self.black_short_castling = True
+        if 'q' in fen_parts[2]:
+            self.black_long_castling = True
+
+        if fen_parts[3] != '-':
+            letter = fen_parts[3][0]
+            digit = fen_parts[3][1]
+            coord_x = from_letter_to_digit[letter]
+            coord_y = 7 - int(digit)
+            self.en_passant_cell = Coord(coord_x, coord_y)
+
+        if fen_parts[4] == '-':
+            self.moves_without_pawn = 0
+        else:
+            self.moves_without_pawn = int(fen_parts[4])
+
+        self.move_clock = int(fen_parts[5])
--- src/position.py
+++ src/position.py
@@ -1,4 +1,5 @@
 """Модуль, который содержит реализацию шахматной позиции."""
+from src.fen_state import FenState
 
 
 class Position:
@@ -11,17 +12,33 @@ class Position:
             fen: Строка, представляющая из себя нотацию Форсайта — Эдвардса.
         """
         self._fen = fen
-        self._board = None
+        self._board = [['-'] * 8 for _ in range(8)]
+        self._fet_state = FenState(fen)
         self._white_figures = None
         self._black_figures = None
         self._white_coords = None
         self._black_coords = None
+        self.generate_board_from_fen()
 
     def generate_board_from_fen(self) -> None:
-        pass
+        board_parts = self._fen.split(' ')[0].split('/')
+        for i, part in enumerate(board_parts):
+            j = 0
+            for cell in part:
+                if cell.isdigit():
+                    j += int(cell)
+                else:
+                    self._board[i][j] = cell
+                    j += 1
 
     def generate_fen_from_board(self) -> None:
         pass
 
     def generate_moves(self) -> None:
         pass
+
+
+if __name__ == "__main__":
+    pos = Position("rnbq1bnr/pppkpppp/8/3p4/4P3/5N2/PPPP1PPP/RNBQKB1R w KQ - 2 3")
+    for partt in pos._board:
+        print(partt)
